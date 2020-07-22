cfe1d47b34de010edebfd706566e9a4c8768f999
Jessica Strachan <jstrachan@rawnet.com>
2020-07-22 09:19:54 +0100

    --wip-- [skip ci]

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Container from "../components/container"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <Container>
+      <div style={{ color: `purple`}}>
+        <Link to="/contact/">Contact</Link>
+        <Link to="/about/">About</Link>
+        <a href="https://twitter.com/JessMadeline" target="_blank">Twitter</a>
+        <Header headerText="Home" />
+        <p>What a world</p>
+        <img src="https://source.unsplash.com/random/400x200" alt="" />
+      </div>
+    </Container>
+    );
 }
